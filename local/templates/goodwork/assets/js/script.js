189bdfd50739f690b135075a98bbd6e28987f782
Rastvor1tel <solvent71@yandex.ru>
2020-04-30 12:11:07 +0300

    Правка чужих скриптов

--- local/templates/goodwork/assets/js/script.js
+++ local/templates/goodwork/assets/js/script.js
@@ -1,12 +1,16 @@
 $(function () {
     
-})
+});
 
 document.addEventListener("DOMContentLoaded", () => {
 	var orderButton = document.querySelector('.button__wrap .js-order');
 	var orderPopUp = document.querySelector('.order__popup.js-order');
 
-	orderButton.addEventListener('click', function(){
-		orderPopUp.classList.add("_visible");
-	})
+	if (orderButton) {
+		orderButton.addEventListener('click', function () {
+			if (orderPopUp) {
+				orderPopUp.classList.add("_visible");
+			}
+		})
+	}
 });
